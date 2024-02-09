6ab6e3311ad3be7fae59dcdb94bdb16338a35923
bkylee <brian.ky.lee@outlook.com>
2024-02-08 21:48:59 -0500

    tested app

--- function_app.py
+++ function_app.py
@@ -1,78 +1,38 @@
-""" import azure.functions as func
 import logging
-
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-
-@app.cosmos_db_output(
-    arg_name="outputDocument",
-    database_name="my-database",
-    collection_name="my-container",
-    connection_string_setting="CosmosDbConnectionString",
-)
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
-
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
-
-    if name:
-        return func.HttpResponse(
-            f"Hello, {name}. This HTTP triggered function executed successfully."
-        )
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200,
-        )
- """
-
+import os
 import azure.functions as func
-import logging
+from azure.cosmos import CosmosClient, PartitionKey
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
-@app.queue_output(
-    arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage"
-)
-@app.cosmos_db_output(
-    arg_name="outputDocument",
-    database_name="my-database",
-    container_name="my-container",
-    connection="CosmosDbConnectionString",
-)
-def test_function(
-    req: func.HttpRequest,
-    msg: func.Out[func.QueueMessage],
-    outputDocument: func.Out[func.Document],
-) -> func.HttpResponse:
+@app.function_name(name="visitorCount")
+@app.route(route="visitorCount")
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
-    logging.info("Python Cosmos DB trigger function processed a request.")
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
 
-    if name:
-        outputDocument.set(func.Document.from_dict({"id": name}))
-        msg.set(name)
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-            "Please pass a name on the query string or in the request body",
-            status_code=400,
-        )
+    # Initialize Cosmos Client
+    # create env vars by using export varname=whatever
+    # i also created app settings for these
+    url = os.getenv("COSMOSDB_URL")
+    key = os.getenv("COSMOSDB_KEY")
+    client = CosmosClient(url, credential=key)
+
+    # Select database
+    database_name = "my-database"
+    database = client.get_database_client(database_name)
+
+    # Select Container
+    container_name = "CRC-visitor count"
+    container = database.get_container_client(container_name)
+
+    # Read the item ( item = id, partition key = 'visitorCount )
+    item_response = container.read_item(item="3", partition_key="test")
+
+    # Increment the counter
+    item_response["count"] += 1
+
+    # Update the item
+    container.upsert_item(item_response)
+
+    return func.HttpResponse(f"Visitor count: {item_response['count']}")
